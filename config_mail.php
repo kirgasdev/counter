76ab34aa5a8381e897afab581775a740c5758657
kirgasit <itadmin@kirgas.com>
2019-01-21 14:38:39 +0200

    First commit

---
+++ config_mail.php
@@ -0,0 +1,15 @@
+<?php
+$data = array(
+	'email' => array(
+		'hostname' => '{pop3.server:110/pop3/novalidate-cert}INBOX',
+		'username' => 'login@domain.com',
+		'password' => 'pass'
+	),
+	'pagination' => array(
+		'sort' => 'DESC',
+		'limit' => 1000000,
+		'offset' => 0
+	)
+	);
+
+?>
---
+++ config_sms.php
@@ -0,0 +1,7 @@
+<?php
+// Подключение к MySQL
+$servername = "server_IP"; // сервер
+$username = "login"; // имя пользователя
+$password = "pass"; // пароль  
+$dbname = "database"; // база данных
+?>
